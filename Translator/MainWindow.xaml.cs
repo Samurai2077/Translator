361a6a260b2aa6ee0f41e8df5f3ffd7ca48ee8c0
Артём Ражиев <temka24042000@mail.ru>
2020-09-23 16:10:37 +0300

    Релиз!

--- Translator/MainWindow.xaml.cs
+++ Translator/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Translator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Word currentWord;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +30,7 @@ namespace Translator
 
         private void Initialize()
         {
-
+            ShowNextWord();
         }
 
         public void AddWord(object sender, RoutedEventArgs e)
@@ -36,12 +38,6 @@ namespace Translator
             AddWordForm addWordForm = new AddWordForm();
             addWordForm.Owner = this;
             addWordForm.ShowDialog();
-
-            using (var context = new WordContext())
-            {
-                Word lastWord = (from element in context.Words select element).ToList().Last();
-                mainWord.Content = lastWord.FirstForm;
-            }
         }
 
         private void ShowAllWords(object sender, RoutedEventArgs e)
@@ -49,6 +45,71 @@ namespace Translator
             ShowAllWordsForm form = new ShowAllWordsForm();
             form.Owner = this;
             form.Show();
+        }
+
+        private void ShowNextWord()
+        {
+            ResetColorTextBox();
+            ResetRightAnswerLabel();
+            ResetContentTextBox();
+
+            checkButton.Visibility = Visibility.Visible;
+            nextButton.Visibility = Visibility.Hidden;
+            using (var context = new WordContext())
+            {
+                if (context.Words.Count() == 0)
+                    return;
+
+                Random random = new Random((int)DateTime.Now.Ticks);
+                currentWord = context.Words.ToList()[random.Next(context.Words.Count())];
+
+                mainWord.Content = currentWord.FirstForm;
+            }
+        }
+
+        private void ShowNextWord(object sender, RoutedEventArgs e)
+        {
+            ShowNextWord();
+        }
+
+        private void CheckAnswer(object sender, RoutedEventArgs e)
+        {
+            CheckAnswer();
+        }
+
+        private void CheckAnswer()
+        {
+            checkButton.Visibility = Visibility.Hidden;
+            nextButton.Visibility = Visibility.Visible;
+
+            translateTextBox.Background = translateTextBox.Text == currentWord.Translate ? Brushes.Green : Brushes.Red;
+            secondFormTextBox.Background = secondFormTextBox.Text == currentWord.SecondForm ? Brushes.Green : Brushes.Red;
+            thirdFormTextBox.Background = thirdFormTextBox.Text == currentWord.ThirdForm ? Brushes.Green : Brushes.Red;
+
+            rightTranslateLabel.Content = currentWord.Translate;
+            rightSecondFormLabel.Content = currentWord.SecondForm;
+            rightThirdFormLabel.Content = currentWord.ThirdForm;
+        }
+
+        private void ResetColorTextBox()
+        {
+            translateTextBox.Background = Brushes.White;
+            secondFormTextBox.Background = Brushes.White;
+            thirdFormTextBox.Background = Brushes.White;
+        }
+
+        private void ResetContentTextBox()
+        {
+            translateTextBox.Text = string.Empty;
+            secondFormTextBox.Text = string.Empty;
+            thirdFormTextBox.Text = string.Empty;
+        }
+
+        private void ResetRightAnswerLabel()
+        {
+            rightTranslateLabel.Content = string.Empty;
+            rightSecondFormLabel.Content = string.Empty;
+            rightThirdFormLabel.Content = string.Empty;
         }
     }
 }
